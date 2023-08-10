c1807252bbf17b88a0a014875756d79b2cb2f733
rabib-jahin <rabibjahin2000@gmail.com>
2023-08-10 13:24:49 +0600

    subscription added

---
+++ model/subscription.js
@@ -0,0 +1,37 @@
+const { sq } = require("../repository/base");
+const { DataTypes, Sequelize } = require("sequelize");
+const Auth =require("./auth")
+
+const Subscription = sq.define("subscription", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING
+    },
+    fee: {
+        type: Sequelize.STRING
+    },
+   
+    logo:{
+        type: Sequelize.STRING
+    }
+
+  });
+
+  Subscription.hasMany(Auth, {
+    foreignKey: 'sub_id', // This will be the foreign key in the 'Course' model
+    onDelete: 'CASCADE', // This will delete associated courses when a teacher is deleted
+  });
+  
+  Auth.belongsTo(Subscription, {
+    foreignKey: 'sub_id', // This should match the foreign key defined in the 'Teacher' model
+  });
+
+  Subscription.sync().catch(err=>{
+    console.log(err)
+  })
+
+  module.exports = Subscription;
