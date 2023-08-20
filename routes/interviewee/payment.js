d6f62e85e70391ed14011228ca1cc23c17b954df
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-08-20 10:45:15 +0600

    payment redirection test

--- routes/interviewee/payment.js
+++ routes/interviewee/payment.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 //sslcommerz ipn
 router.post('/ipn', (req,res) => {
+    console.log(req.headers)
     console.log(req.query)
     console.log(req.params)
     console.log(req.body)
