88de2dece0c10855814421317868ae113471ef74
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-09-10 11:36:24 +0600

    prod back done

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -4,6 +4,7 @@ const MonitorController=require('../controllers/monitor').MonitorController
 // const monitorController=new MonitorController()
 
 router.route("/github/backend").post(MonitorController.gitBackendWebhook);
+router.route("/prodback").get(MonitorController.awsProdBack);
 router.route("/connect").get(MonitorController.connect);
 router.route("/initialize").get(MonitorController.initialize);
 
