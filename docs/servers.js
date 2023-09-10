e5fadccfab13815b23eb2cfde7b1eeb6400b4f4f
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-09-10 21:23:23 +0600

    chng

--- docs/servers.js
+++ docs/servers.js
@@ -2,7 +2,7 @@ module.exports = {
     servers:[
         {
             url:'http://43.224.110.132/api/v1.0.0/'
-        },
+        },//ss
         {
             url:`http://localhost:${process.env.PORT || 4000}/api/v1.0.0/`
         }
